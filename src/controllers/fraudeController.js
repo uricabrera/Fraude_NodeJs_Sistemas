4a10482add0f74b63f089e387798dbda23c8bc34
Uriel Cabrera <59104218+uricabrera@users.noreply.github.com>
2024-11-08 11:56:04 -0300

    Update fraudeController.js

--- src/controllers/fraudeController.js
+++ src/controllers/fraudeController.js
@@ -4,9 +4,16 @@ const axios = require('axios');
 const detectarFraudePorFecha = (ventas) => {
   const conteoPorFecha = {};
 
+  console.log("Estoy dentro de la funcion detectarFraudePorFecha");
+  console.log(ventas);
+
   for (let venta of ventas) {
     const userId = venta.id;
     const fecha = venta.fecha;
+
+    console.log("Estoy dentro del for loop");
+    console.log(venta.id);
+    console.log(venta.fecha);
 
     // Inicializa el objeto si no existe
     if (!conteoPorFecha[userId]) {
@@ -15,6 +22,9 @@ const detectarFraudePorFecha = (ventas) => {
     if (!conteoPorFecha[userId][fecha]) {
       conteoPorFecha[userId][fecha] = 0;
     }
+
+    console.log("Estoy viendo el conteoPorFecha");
+    console.log(conteoPorFecha);
 
     // Aumenta el contador de órdenes para el usuario en esa fecha
     conteoPorFecha[userId][fecha] += 1;
@@ -36,6 +46,9 @@ const verificarFraude = async (req, res) => {
     const response = await axios.get('https://fraudeapi01-braxdrdjc4f4c2hr.centralus-01.azurewebsites.net/api/v1/orders/${user_id}'); 
 // Llamada al microservicio de ventas
     const ventas = response.data;
+
+    console.log("Estoy en el verificarFraude y el valor de ventas es ");
+    console.log(ventas);
 
     // Aplicar el algoritmo de detección de fraude
     const esFraude = detectarFraudePorFecha(ventas);
